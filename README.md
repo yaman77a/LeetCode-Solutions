b043ba906fd1805ee98bf012d5b603adfba9f1ef
Yaman Aydıner <yamanaydiner@Yaman-MacBook-Pro.local>
2025-02-16 20:53:34 +0100

    Added Two Sum II to README and uploaded solution

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ This repository contains my solutions to LeetCode problems, written in Python. S
 - **Time Complexity**: **O(n)**
 - **Code:** [View Solution](easy/lemonade_change.py)
 
+---
+
+## Medium Problems:
+
+**Leetcode 167 - Two Sum II**
+- **Problem**: Given a sorted array, find two numbers such that they add up to a target.
+- **Solution**: **two-pointer**
+- **Complexity**: **O(n)**
+- **Code:** [View Solution](medium/two_sum_ii.py)
--- medium/two_sum_ii.py
+++ medium/two_sum_ii.py
@@ -1,12 +1,12 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        total = None
-        l, r= 0, len(numbers) - 1
-        while r > l:
-            total = numbers[l] + numbers[r]
-            if total > target:
-                r -= 1
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        total = None # Stores the sum of current two numbers
+        l, r = 0, len(numbers) - 1 # Two pointers at both end of the list
+        while r > l: # A loop while left pointer is before right pointer
+            total = numbers[l] + numbers[r] # Compute the sum of two items            
+            if total > target:    
+                r -= 1 # If sum is larger than target, then move right pointer left
             elif total < target:
-                l += 1
+                l += 1 # If sum is smaller than targer, then move left pointer right 
             else:
-                return [l + 1, r + 1]
+                return [l + 1, r + 1] # When we found the target sum, return 1-based indices
